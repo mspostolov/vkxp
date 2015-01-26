e92a26234d4265afc683e8e5dac9df5431daebf6
fortis <fortis@376960.no-reply.drupal.org>
2015-01-26 11:47:10 +0530

    Issue #2409779 by fortis, Niklan, Spleshka: Backport to Drupal 6

--- vkxp.api.php
+++ vkxp.api.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * @file
+ * @file vkxp.api.php
  * Hooks provided by the vkxp module.
  */
- 
- 
+
 /**
- * Allows to change http query params
+ * Allows to change http query params.
+ *
  * $query contains 3 values:
  *   @param $query['method']: string with vk api method name.
  *   @param $query['params']: array with parameters that will be posted.
